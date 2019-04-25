ec3b1c4823af2f5875af3f3c06dff0856c29b6c9
Kiko Valera <kikovalera@gmail.com>
2019-04-25 19:15:37 +0800

    started home page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index Page</title>
+	<link rel="stylesheet/less"  href="/css/styles.less">
+	<script src="/js/less.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Noticia+Text" rel="stylesheet">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="main">
+			<nav id="nav" class="normal">
+				<div class="container">
+					<a href="#" class="left">
+						<h2>Nicola Rider</h2>
+						<h3>Fashion Editor/Stylelist</h3>
+					</a>
+					<div class="right nav-links">
+						<a href="#">HOME</a>
+						<a href="#">RESUME</a>
+						<a href="#">PROJECTS</a>
+						<a href="#">CONTACT</a>
+					</div>
+				</div>
+			</nav>
+			<section class="welcome">
+				<div class="column left">
+					<div class="img-container">
+						<img src="/images/imageito.jpg">	
+					</div>
+					
+				</div>
+				<div class="column right">
+					<div class="header-text">
+						<h3>Hello,</h3>
+						<h4>a bit about me:</h4>
+					</div>
+					<div class="circle-container">
+						<div class="circle one"><p>My Resume</p></div>
+						<div class="circle two"><p>My Work</p></div>
+						<div class="circle three"><p>My Skills</p></div>
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</section>
+
+
+		</div>
+
+		
+	</div>
+	<footer>
+		<div class="footer-wrapper">
+			<div class="footer">
+				<div class="img-container">
+					<img src="/images/footers/email.png">
+				</div>
+				<div class="text">
+					<h3>Call</h3>
+					<p>T: 123-456-789</p>
+					<p>F: 123-456-789</p>
+				</div>
+				</div>
+				<div class="footer">
+					<div class="img-container">
+						<img src="/images/footers/email.png">
+					</div>
+					<div class="text">
+						<h3>Contact</h3>
+						<p>T: 123-456-789</p>
+						<a href="#">info@mysite.com</a>
+					</div>
+				</div>
+				<div class="footer">
+					<div class="img-container">
+						<img src="/images/footers/email.png">
+					</div>
+					<div class="text">
+						<h3>follow me</h3>
+						<div class="social-media-links">
+							<a href="https://www.facebook.com/manilaoceanpark/" target="_black" class="fb">
+								<img src="/images/footers/facebook_icon.svg">
+							</a>
+							<a href="https://www.facebook.com/manilaoceanpark/" target="_black" class="fb">
+								<img src="/images/footers/google_icon.svg">
+							</a>
+							<a href="https://www.facebook.com/manilaoceanpark/" target="_black" class="fb">
+								<img src="/images/footers/twitter_icon.svg">
+							</a>
+
+						</div>
+					</div>
+				</div>
+				<div class="footer">
+					<div class="img-container">
+						<img src="/images/footers/email.png">
+					</div>
+					<div class="text">
+						<p>© 2023 by Nicola Rider.</p>
+						<a href="#">Proudly created with Wix.com</a>
+					</div>
+				</div>
+		</div>	
+	</footer>
+</body>
+</html>
